3253490dc1e9e454f7c22b6cbc9123916f2fd525
unknown <youssef.ojeil@hotmail.com>
2023-01-20 15:55:36 -0500

    create home page and create post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { Home, CreatePost } from "./pages";
+import { logo } from "./assets";
 
 const App = () => {
   return (
