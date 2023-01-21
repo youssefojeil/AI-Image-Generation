b214d1cb72701b91e4d6fbf7d2760b870e89f770
unknown <youssef.ojeil@hotmail.com>
2023-01-21 15:55:33 -0500

    create post route to to generate openai image based off user prompt

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,9 +1,45 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { v2 as cloudinary } from "cloudinary";
+import { Configuration, OpenAIApi } from "openai";
 
 import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from Dalle");
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    console.log(req.body);
+    const { prompt } = req.body;
+    console.log(prompt);
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    console.log(aiResponse);
+    const image = aiResponse.data.data[0].b64_json;
+    console.log(image);
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
+
+export default router;
