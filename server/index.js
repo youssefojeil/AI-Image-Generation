c164782463ad35a9a5a8d122e995f43ff6b652a6
unknown <youssef.ojeil@hotmail.com>
2023-01-21 15:39:10 -0500

    create mongodb data base and connect

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+
 dotenv.config();
 
 const app = express();
@@ -13,9 +15,15 @@ app.get("/", async (req, res) => {
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log(`server has started on port http://localhost:8080`)
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+
+    app.listen(8080, () =>
+      console.log(`server has started on port http://localhost:8080`)
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 startServer();
