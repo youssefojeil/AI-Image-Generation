019d438731d0ee3403267f0b5fa02ab92eeb9590
unknown <youssef.ojeil@hotmail.com>
2023-01-20 19:55:34 -0500

    setup express server

--- server/index.js
+++ server/index.js
@@ -3,3 +3,19 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.send("Hello from Dall-e");
+});
+
+const startServer = async () => {
+  app.listen(8080, () =>
+    console.log(`server has started on port http://localhost:8080`)
+  );
+};
+
+startServer();
